c730dd476805e2583afd73a1136bc33d9bd94980
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-06-10 23:09:53 -0700

    fonts import

---
+++ src/fonts/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+import UnifrakturMaguntia from './UnifrakturMaguntia.woff2';
+import MedievalSharp from './MedievalSharp.woff2';
+import IndieFlower from './IndieFlower.woff2';
+import VT323 from './VT323.woff2';
